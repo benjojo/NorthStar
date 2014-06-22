8e98cd2bc298c32056ac505ce1db02782ec2e5ed
Ben Cox <Ben@Benjojo.co.uk>
2014-06-22 18:44:23 +0100

    Added challenge / Responce code to connection handlers

--- netcore.go
+++ netcore.go
@@ -1,10 +1,58 @@
 package main
 
 import (
+	"errors"
 	"net"
-	"sync"
 )
 
 func WatchForNewPeers(inbound chan string) {
 
+}
+
+func WaitForConnections() {
+	l, err := net.Listen("tcp", "0.0.0.0:48563")
+	if err != nil {
+		logger.Fatalf("Could not start listening on TCP side (%s)", err)
+	}
+	defer l.Close()
+
+	logger.Println("TCP Side is ready for connections.")
+
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			logger.Println("Error accepting TCP: ", err.Error())
+		}
+		// Handle connections in a new goroutine.
+		go HandlePeerConn(conn)
+	}
+}
+
+func HandlePeerConn(conn net.Conn) {
+	err := ChallengeClient(conn)
+	if err != nil {
+		conn.Close()
+	}
+	defer conn.Close()
+	payload := make([]byte, 64000)
+	for {
+		conn.Read(payload)
+	}
+}
+
+func ChallengeClient(conn net.Conn) (err error) {
+	Challenge := RandString(32)
+	conn.Write(EncryptText([]byte("NS "+Challenge), []byte(CC_KEY)))
+	Responce := make([]byte, 64)
+	back, e := conn.Read(Responce)
+	if e != nil {
+		return e
+	}
+	decrypted := DecryptText(Responce[:back], []byte(CC_KEY))
+	if string(decrypted) == Challenge {
+		return err
+	} else {
+		return errors.New("AAAAAAAAAA")
+	}
 }
