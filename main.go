9d42698dfd7654c36c6747469d80d0f37707bf17
Ben Cox <Ben@Benjojo.co.uk>
2014-06-22 18:44:03 +0100

    Make sure the crypto key is gobal

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 )
 
+var CC_KEY string = ""
+
 func main() {
 	SetupLogger()
 	key := flag.String("key", "", "Set this as a long string that only you and the rest of your nodes know")
@@ -11,6 +13,7 @@ func main() {
 	if *key == "" || *key == "InsertLongKeyHere" {
 		logger.Fatal("No key or a too short key use -key=\"InsertLongKeyHere\"")
 	}
+	CC_KEY = *key
 
 	logger.Printf("NorthStar - DHT Configuration Tool.")
 }
