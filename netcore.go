ca9a3262b5f6372c193471b746efa631a6ce9251
Ben Cox <ben@benjojo.co.uk>
2014-06-29 20:21:26 +0100

    Moved the handshake + challenge to its own file
    This is to make it easier to review in the future.

--- netcore.go
+++ netcore.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"net"
 )
 
@@ -40,31 +39,3 @@ func HandlePeerConn(conn net.Conn) {
 		conn.Read(payload)
 	}
 }
-
-func ChallengeClient(conn net.Conn) (err error) {
-	Challenge := RandString(32)
-	conn.Write(EncryptText([]byte("NS "+Challenge), []byte(CC_KEY)))
-	Responce := make([]byte, 64)
-	back, e := conn.Read(Responce)
-	if e != nil {
-		return e
-	}
-	decrypted := DecryptText(Responce[:back], []byte(CC_KEY))
-	if string(decrypted) == Challenge {
-		return err
-	} else {
-		return errors.New("AAAAAAAAAA")
-	}
-}
-
-/*
-
-Connect
-S->C "NS"
-C->S "{Cypto Blob containing Time and RandomString}"
-S->C "{Cypto Blob containing the hashed version of the random string and the time}"
-C->S "{ACK packet with hashed version of the decrypted GOB}"
-S->C "{Ask for Hostname and other info}"
-C->S "{GOB containing host info}"
--- Normal Relay mode starts --
-*/
