2ce5b76345c7d155815eb4909e9da471e3b372de
Ben Cox <Ben@Benjojo.co.uk>
2014-07-12 22:30:49 +0100

    Fixed issue where debuglogger was not defined :|

--- logging.go
+++ logging.go
@@ -12,8 +12,8 @@ var debuglogger *log.Logger
 func SetupLogger(debug bool) {
 	logger = log.New(os.Stdout, "[Main] ", log.Lshortfile|log.Ltime)
 	if debug {
-		logger = log.New(os.Stdout, "[Debug] ", log.Lshortfile|log.Ltime)
+		debuglogger = log.New(os.Stdout, "[Debug] ", log.Lshortfile|log.Ltime)
 	} else {
-		logger = log.New(ioutil.Discard, "[Debug] ", log.Lshortfile|log.Ltime)
+		debuglogger = log.New(ioutil.Discard, "[Debug] ", log.Lshortfile|log.Ltime)
 	}
 }
