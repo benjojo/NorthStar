86029110431d3e3da5bf53025d81c6579d96d2b6
Ben Cox <ben@benjojo.co.uk>
2014-07-21 22:15:45 +0100

    Added pprof debugging

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"io/ioutil"
+	"os"
+	"runtime/pprof"
 	"time"
 )
 
@@ -13,10 +15,22 @@ func main() {
 	key := flag.String("key", GetFileOrBlank("/.nskey"), "Set this as a long string that only you and the rest of your nodes know")
 	seed := flag.String("seed", "", "Set this to the key of the network, Need atleast 1 matchine on the network to have this")
 	dhtpos := flag.Bool("dhtdefault", false, "If you cannot get announcing to work, then set this to true")
+	cpuprofile := flag.String("cpuprofile", "", "write cpu profile to file")
 	debug := flag.Bool("debug", false, "Enable for debug text")
 	flag.Parse()
-	go ListenForIRCConnections()
+
 	SetupLogger(*debug)
+	go ListenForIRCConnections()
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			logger.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	if *key == "" || *key == "InsertLongKeyHere" || len(*key) < 8 {
 		logger.Fatal("No key or a too short key use -key=\"InsertLongKeyHere\"")
 	}
