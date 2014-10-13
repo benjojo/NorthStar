4d51f56d9256c3037841286eb944b9d49e35e105
Ben Cox <Ben@Benjojo.co.uk>
2014-10-13 20:48:46 +0100

    Track version upgrade

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ func main() {
 	for {
 		time.Sleep(time.Second * 10)
 		Holla := PeerPacket{}
-		Holla.Message = "Hi everyone G:6afef92d"
+		Holla.Message = "Hi everyone G:7a37e0bd"
 		Holla.Service = "Holla"
 		SendPacket(Holla)
 	}
